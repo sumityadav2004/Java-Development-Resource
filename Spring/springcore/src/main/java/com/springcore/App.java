c4abd6c57cbd96d9e38dc94d4c9d16948e0a7c26
sumit yadav <sumityadav70539160@gmail.com>
2026-03-15 22:47:49 +0530

    Spring Basic

--- Spring/springcore/src/main/java/com/springcore/App.java
+++ Spring/springcore/src/main/java/com/springcore/App.java
@@ -13,7 +13,10 @@ public class App
     {
         System.out.println( "Hello World!" );
     ApplicationContext context=new ClassPathXmlApplicationContext("configure.xml");
+    
           student student1=  (student)context.getBean("student1");
+          student student2=    (student)context.getBean("student2");
           System.out.println(student1);
+          System.out.println(student2);
     }
 }
